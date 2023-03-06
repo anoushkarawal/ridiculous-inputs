ae206ac777a97d9afdd0113e6083c92bd0d459b6
anoushkarawal <124563263+anoushkarawal@users.noreply.github.com>
2023-03-05 19:43:10 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pop up ads
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/__anoushkaa__/pen/wvEeRdE](https://codepen.io/__anoushkaa__/pen/wvEeRdE).
+
---
+++ dist/script.js
@@ -0,0 +1,59 @@
+var modal = document.getElementById("adModal");
+var span = document.getElementsByClassName("close")[0];
+
+function myFunction() {
+    $duration = 5;
+			$('.seconds').text($duration);
+			$myTimer = setInterval(function(){ startTimer() }, 1000);
+
+			
+			function startTimer(){
+				$duration--;
+				$('.seconds').text($duration);
+				if($duration==0){
+					clearInterval($myTimer);
+
+				}
+			}
+  setTimeout(()=>{
+    span.disabled = false;			
+    
+    console.log('Button Activated')}, 5000)
+    span.disabled = true;
+    span.onclick = function() {
+        modal.style.display = "none";
+}
+
+}
+
+
+
+var typingTimer;               
+var doneTypingInterval = 3000; 
+
+
+var $input = $('#InputName'); // default value
+
+function clickedinput(element){
+    console.log($input);
+    $input = $('#'+element);
+
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
+
+function doneTyping () {
+  modal.style.display = "block";
+  myFunction();
+}
+}
+function submitbutton(){
+    alert("Your form has been submitted");
+}
